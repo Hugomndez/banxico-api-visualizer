94e93524df7097b3cb6e80835ae069442fff1bc2
Hugomndez <hello@hugomendez.dev>
2022-03-03 19:03:13 -0600

    Added Form input logic

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,47 @@
+import { useState } from 'react';
 import './Form.css';
 
 const Form = () => {
+  const [newTokenValue, setNewTokenValue] = useState('');
+  const [newSeriesValue, setNewSeriesValue] = useState('');
+
+  const onChangeToken = event => {
+    setNewTokenValue(event.target.value);
+  };
+
+  const onChangeSeries = event => {
+    setNewSeriesValue(event.target.value);
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    if (!newTokenValue || !newSeriesValue) {
+      return null;
+    }
+    console.log(newTokenValue);
+    console.log(newSeriesValue);
+  };
+
+  const onKeyUp = event => {
+    if (event.charCode === 13) {
+      onSubmit(event);
+    }
+  };
+
   return (
-    <form id='apiForm'>
+    <form id='apiForm' onSubmit={onSubmit} onKeyPress={onKeyUp}>
       <fieldset>
         <label htmlFor='token'>Token</label>
-        <input itemType='text' id='token'></input>
+        <input
+          itemType='text'
+          id='token'
+          value={newTokenValue}
+          onChange={onChangeToken}
+          placeholder='Add Your Token'
+          minLength={64}
+          maxLength={64}
+          required
+        ></input>
         <p>
           You need a Banxico token. You can find it on:{' '}
           <a href='https://www.banxico.org.mx/SieAPIRest/service/v1/token'>
@@ -15,10 +51,17 @@ const Form = () => {
       </fieldset>
       <fieldset>
         <label htmlFor='series'>Series</label>
-        <input itemType='text' id='series'></input>
+        <input
+          itemType='text'
+          id='series'
+          value={newSeriesValue}
+          onChange={onChangeSeries}
+          placeholder='Add Series Number'
+          required
+        ></input>
         <p>Example: SF61745, SP68257</p>
       </fieldset>
-      <button>Fetch</button>
+      <button type='submit'>Fetch</button>
     </form>
   );
 };
