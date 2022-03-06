00e9ba4c7a4b4d3bf04926525b55c23264bc1a73
Hugomndez <hello@hugomendez.dev>
2022-03-06 14:37:31 -0600

    Added useFetch Hook.

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState, useRef } from 'react';
+
+const useFetch = (url, options) => {
+  const cache = useRef({});
+  const [status, setStatus] = useState('idle');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (!url) return;
+    const fetchData = async () => {
+      setStatus('fetching');
+      if (cache.current[url]) {
+        const data = cache.current[url];
+        setData(data);
+        setStatus('fetched');
+      } else {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        cache.current[url] = data;
+        setData(data);
+        setStatus('fetched');
+        console.log(data);
+      }
+    };
+
+    fetchData();
+  }, [url, options]);
+
+  return { status, data };
+};
+
+export default useFetch;
