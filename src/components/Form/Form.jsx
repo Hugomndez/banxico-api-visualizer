b54a73241310b0a832c2431e3922572403b60c33
Hugomndez <hello@hugomendez.dev>
2022-03-06 14:36:45 -0600

    Refactored form code.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,95 +1,96 @@
 import { useState } from 'react';
+import { useFetch } from '../../hooks/';
 import './Form.css';
 
 const Form = () => {
-  const [Token, setNewToken] = useState('');
-  const [Series, setNewSeries] = useState('');
-  const [data, setData] = useState(null);
+  const [query, setQuery] = useState('');
+  const [token, setToken] = useState('');
 
-  const API_URL = (series, token) =>
-    `https://www.banxico.org.mx/SieAPIRest/service/v1/series/${series}?token=${token}`;
+  const API_URL =
+    query &&
+    `https://5i8qcjp333.execute-api.us-east-1.amazonaws.com/dev/series/${query}?token=${token}`;
 
-  const API_URL_SERIES = API_URL(Series, Token);
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      Authorization:
+        '01f04831044f073702d9244604d41c055e7c14bb96218e169926482fb5699788',
+    },
+  };
 
-  const fetchData = (url, options = null) => {
-    try {
-      fetch(url, options)
-        .then(res => {
-          if (!res.ok) {
-            throw new Error('Network response was not OK');
-          }
-          return res.json();
-        })
-        .then(data => {
-          setData(data);
-          console.log(data);
-        });
-    } catch (error) {
-      console.error(
-        'There has been a problem with the fetch operation:',
-        error
-      );
+  const { status, data } = useFetch(API_URL, fetchOptions);
+
+  const onKeyEnter = async event => {
+    if (event.key === 'Enter') {
+      await handleSubmit(event);
     }
-    return { data };
   };
 
-  const onChangeToken = event => {
-    setNewToken(event.target.value);
-  };
-
-  const onChangeSeries = event => {
-    setNewSeries(event.target.value);
-  };
-
-  const onSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (!Token && !Series) {
+
+    const tokenToFetch = event.currentTarget.token.value;
+    const seriesToFetch = event.currentTarget.series.value;
+
+    if (!tokenToFetch && !seriesToFetch) {
       return null;
     }
-    fetchData(API_URL_SERIES);
-  };
 
-  const onKeyUp = event => {
-    if (event.key === 'Enter') {
-      onSubmit(event);
-    }
+    setQuery(seriesToFetch);
+
+    setToken(tokenToFetch);
+
+    event.currentTarget.series.value = '';
   };
 
   return (
-    <form id='apiForm' onSubmit={onSubmit} onKeyPress={onKeyUp}>
-      <fieldset>
-        <label htmlFor='token'>Token</label>
-        <input
-          itemType='text'
-          id='token'
-          value={Token}
-          onChange={onChangeToken}
-          placeholder='Add Your Token'
-          minLength={64}
-          maxLength={64}
-          required
-        ></input>
-        <p>
-          You need a Banxico token. You can find it on:{' '}
-          <a href='https://www.banxico.org.mx/SieAPIRest/service/v1/token'>
-            https://www.banxico.org.mx/SieAPIRest/service/v1/token
-          </a>
-        </p>
-      </fieldset>
-      <fieldset>
-        <label htmlFor='series'>Series</label>
-        <input
-          itemType='text'
-          id='series'
-          value={Series}
-          onChange={onChangeSeries}
-          placeholder='Add Series Number'
-          required
-        ></input>
-        <p>Example: SF61745, SP68257</p>
-      </fieldset>
-      <button type='submit'>Fetch</button>
-    </form>
+    <>
+      <form id='apiForm' onSubmit={handleSubmit} onKeyPress={onKeyEnter}>
+        <fieldset>
+          <label htmlFor='token'>Token</label>
+          <input
+            itemType='text'
+            id='token'
+            name='token'
+            placeholder='Add Your Token'
+            minLength={64}
+            maxLength={64}
+            required
+            autoFocus
+          ></input>
+          <p>
+            You need a Banxico token. You can find it on:{' '}
+            <a href='https://www.banxico.org.mx/SieAPIRest/service/v1/token'>
+              https://www.banxico.org.mx/SieAPIRest/service/v1/token
+            </a>
+          </p>
+        </fieldset>
+        <fieldset>
+          <label htmlFor='series'>Series</label>
+          <input
+            itemType='text'
+            id='series'
+            name='series'
+            placeholder='Add Series Number'
+            required
+          ></input>
+          <p>Example: SF61745, SP68257</p>
+        </fieldset>
+        <button type='submit'>Fetch</button>
+      </form>
+      {status && <p>{status}</p>}
+      {status === 'fetched' && (
+        <div>
+          {data.bmx.series.map(item => (
+            <div key={item.idSerie}>
+              <p>{item.idSerie}</p>
+              <p>{item.titulo}</p>
+              <p>{item.fechaInicio}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
