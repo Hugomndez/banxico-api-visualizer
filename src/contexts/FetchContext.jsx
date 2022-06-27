a61f0efb1ce91ca927f2c897819c07198cb5f0dc
Hugomndez <hello@hugomendez.dev>
2022-06-27 11:50:01 -0500

    Fix double fetch bug

--- src/contexts/FetchContext.jsx
+++ src/contexts/FetchContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useMemo } from 'react';
 import { useFetch } from '../hooks';
 
 const FetchContext = createContext();
@@ -10,14 +10,18 @@ const FetchProvider = props => {
     query &&
     `https://5i8qcjp333.execute-api.us-east-1.amazonaws.com/dev/series/${query}?token=${token}`;
 
-  const fetchOptions = {
-    method: 'GET',
-    headers: {
-      Authorization:
-        '01f04831044f073702d9244604d41c055e7c14bb96218e169926482fb5699788',
-    },
-  };
-  const { status, data } = useFetch(API_URL, fetchOptions);
+  const options = useMemo(
+    () => ({
+      method: 'GET',
+      headers: {
+        Authorization:
+          '01f04831044f073702d9244604d41c055e7c14bb96218e169926482fb5699788',
+      },
+    }),
+    []
+  );
+
+  const { status, data } = useFetch(API_URL, options);
 
   return (
     <FetchContext.Provider
